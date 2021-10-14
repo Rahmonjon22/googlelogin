cbcf8238b8fb92c47b32520dabb9135f35b58ef9
Rahmonjon22 <rahmonjon2@yahoo.com>
2021-10-14 13:36:06 +0200

    google

---
+++ implement/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//MIDDleWare
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
