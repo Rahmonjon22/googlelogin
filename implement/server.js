7ac91bea70fdbdf4d554f9ad8a8bdea0195b09fe
Rahmonjon22 <rahmonjon2@yahoo.com>
2021-10-14 15:12:03 +0200

    update

--- implement/server.js
+++ implement/server.js
@@ -1,3 +1,4 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 
@@ -5,11 +6,22 @@ const PORT = process.env.PORT || 5000;
 
 //MIDDleWare
 app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cookieParser()); // to store user access token
 
 
 
 app.get('/', (req, res)=>{
     res.render('index')
+})
+
+app.get('/login', (req, res)=>{
+    res.render('login')
+})
+
+app.post('login', (req, res)=>{
+    let token = req.body.token
+    console.log(token)
 })
 
 app.listen(PORT, ()=>{
